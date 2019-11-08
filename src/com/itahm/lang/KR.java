39f7a8741620d9dac5f5c77e576839bac6a2e6de
corebrg2019 <tomato322@hanmail.net>
2019-11-08 16:04:36 +0900

    수정

--- src/com/itahm/lang/KR.java
+++ src/com/itahm/lang/KR.java
@@ -3,12 +3,14 @@ package com.itahm.lang;
 public class KR {
 
 	public final static String INFO_REMOVE_DB = "데이터베이스 정리.";
-	public final static String INFO_NEW_ROLLING = "새로운 롤링 데이터베이스 생성 소요시간."; // Rolling initialized in
+	public final static String INFO_NEW_ROLLING = "새로운 롤링 데이터베이스 생성 소요시간"; // Rolling initialized in
 	public final static String INFO_SMTP_INIT = "ITAhM 이벤트가 SMTP 서버와 연동되었습니다.";
 	public final static String WARNING_SMTP_FAIL = "이벤트 SMTP 전송 실패.";
 	public final static String INFO_SNMP_CHANGE = "SNMP 상태 변화 감지.";
 	public final static String INFO_IF_CHANGE = "인터페이스 상태 변화 감지.";
 	public final static String ERROR_LICENSE_EXPIRE = "라이선스 만료.";
+	public final static String ERROR_LICENSE_MAC = "인증되지 않은 MAC 주소.";
 	public final static String ERROR_CMD_NOT_FOUND = "명령 없는 요청."; //Command not found.
 	public final static String CONSOLE_NODEMANAGER_DOWN = "노드 관리자 중지."; 	//NodeManager down.
+	
 }
